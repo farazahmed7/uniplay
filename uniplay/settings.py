dabba0e789b28fbf3df15599fb7e4e697c66ac36
faraz <faraz.dipsite@gmail.com>
2017-10-30 00:13:47 +0530

    view updated

--- uniplay/settings.py
+++ uniplay/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'django.contrib.auth.backends.ModelBackend',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
