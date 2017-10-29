ac4047a9dc0aa4901038cabacbc962fbe0c29286
faraz <faraz.dipsite@gmail.com>
2017-10-29 15:06:02 +0530

    view updated

--- uniplay/settings.py
+++ uniplay/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'uniplay.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "friend",
-        "USER": "postgres",
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        "NAME": "uniplay",
+        "USER": "super",
+        'PASSWORD': 'uniplayuniplay',
+        'HOST': 'playpact-566.postgres.pythonanywhere-services.com',
+        'PORT': '10566',
     }
 }
 
