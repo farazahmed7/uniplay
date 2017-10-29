bc53643258c2ce7965a3cd0bf88aca4eedd9fbe7
faraz <faraz.dipsite@gmail.com>
2017-10-29 21:45:29 +0530

    view updated

--- uniplay/settings.py
+++ uniplay/settings.py
@@ -127,6 +127,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
